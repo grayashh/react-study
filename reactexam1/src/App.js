afaf0f1b5562a60f7f7f6721f9b0d01dca4c2db9
kimtaekang <k_2684@naver.com>
2022-06-30 22:08:57 +0900

    JSX 문법

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,11 +1,30 @@
-import "./App.css";
+// import "./App.css";
+import MyHeader from "./MyHeader";
+import MyFooter from "./MyFooter";
 
 function App() {
+  let name = "김태강";
+
+  const style = {
+    App: {
+      backgroundColor: "black",
+    },
+    h2: {
+      color: "red",
+    },
+    bold_text: {
+      color: "green",
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>안녕 리액트</h2>
-      </header>
+    <div style={style.App}>
+      <MyHeader />
+      <h2 style={style.h2}>안녕 리액트 {name}</h2>
+      <b style={style.bold_text} id="bold_text">
+        React.js
+      </b>
+      <MyFooter />
     </div>
   );
 }
