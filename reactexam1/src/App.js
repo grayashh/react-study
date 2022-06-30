c6dbd684d2f2e6565c4295f15836861347e1cab7
kimtaekang <k_2684@naver.com>
2022-06-30 23:27:07 +0900

    State

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,29 +1,13 @@
 // import "./App.css";
 import MyHeader from "./MyHeader";
 import MyFooter from "./MyFooter";
+import Counter from "./Counter";
 
 function App() {
-  let name = "김태강";
-
-  const style = {
-    App: {
-      backgroundColor: "black",
-    },
-    h2: {
-      color: "red",
-    },
-    bold_text: {
-      color: "green",
-    },
-  };
-
   return (
-    <div style={style.App}>
+    <div>
       <MyHeader />
-      <h2 style={style.h2}>안녕 리액트 {name}</h2>
-      <b style={style.bold_text} id="bold_text">
-        React.js
-      </b>
+      <Counter />
       <MyFooter />
     </div>
   );
